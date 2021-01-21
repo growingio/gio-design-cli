ab8abb41dcbb1cdee2ee58fe8c83c9c9c5c9e0b9
Pan Jie <panjie@growingio.com>
2021-01-21 20:16:28 +0800

    chore: set up code format and lint tools

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,29 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable no-unused-expressions */
 
-const version = require('../lib/version');
+const version = require("../lib/version");
 
-require('yargs')
+require("yargs")
   .scriptName("gio-design")
-  .usage('Usage: $0 [command] [options]')
-  .command('version', 'Output the version of your package via the command line.', (yargs) => {
-    yargs.option(
-      'package-file',
-      {
-        alias: 'p',
-        type: 'string',
-        describe: 'Specify a package file to parse version'
-      }
-    )
-    .demandOption(['package-file'], 'Please provide package-file parameter')
-  }, function (argv) {
-    console.log(version(argv.p));
-  })
-  .help()
-  .argv
+  .usage("Usage: $0 [command] [options]")
+  .command(
+    "version",
+    "Output the version of your package via the command line.",
+    (yargs) => {
+      yargs
+        .option("package-file", {
+          alias: "p",
+          type: "string",
+          describe: "Specify a package file to parse version",
+        })
+        .demandOption(
+          ["package-file"],
+          "Please provide package-file parameter"
+        );
+    },
+    (argv) => {
+      console.log(version(argv.p));
+    }
+  )
+  .help().argv;
